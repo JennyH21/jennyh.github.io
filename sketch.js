93031208a3fb5b2b7aa943c9824deaf942fa4261
CassidyPong <32074677+CassidyPong@users.noreply.github.com>
2017-11-06 11:48:27 -0800

    java edit

--- sketch.js
+++ sketch.js
@@ -1,15 +1,16 @@
 
           var capture;
 
-          //function setup() {
-        //  createCanvas(390, 240);
-          //capture = createCapture(VIDEO);
-          //capture.size(320, 240);
-          //capture.hide();
-        //  }
-
-          function draw() {
-          background(255);
-          image(capture, 0, 0, 320, 240);
+          function setup() {
+          createCanvas(390, 240);
+          capture = createCapture(VIDEO);
+          capture.size(320, 240);
+          capture.hide();
           filter('INVERT');
-        }
+         }
+
+          //function draw() {
+        //  background(255);
+        //  image(capture, 0, 0, 320, 240);
+          //filter('INVERT');
+      //  }
