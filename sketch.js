f77acd7ca448c5a0448e6801c78d7165f29e40a2
CassidyPong <32074677+CassidyPong@users.noreply.github.com>
2017-11-08 01:02:30 -0800

    screen cap edit

--- sketch.js
+++ sketch.js
@@ -4,13 +4,13 @@
           function setup() {
           createCanvas(390, 240);
           capture = createCapture(VIDEO);
-          capture.size(390, 240);
+          capture.size(320, 240);
           capture.hide();
          }
 
           function draw() {
           background(255);
-          image(capture, 0, 0, 390, 240);
+          image(capture, 0, 0, 320, 240);
           filter('INVERT');
      }
 
